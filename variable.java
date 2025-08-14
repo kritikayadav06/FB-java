4a407873afbc2b3f5753908e9cbb5f4aac81d700
kritikayadav06 <kritikayadav0608@gmail.com>
2025-08-14 10:25:21 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,11 @@
+public class variable {
+     static int a=2; //static variable
+    public static void main(String args[]){
+        int a=10; //Local variable
+        System.out.println(a);
+        System.out.println(variable.a);
+
+       
+    }
+    
+} 
